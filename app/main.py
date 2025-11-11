9873e4e2da9cf9ab26f91fc37896ee3d548f2511
Alexey Dmitriev <lesha1599@mail.ru>
2025-11-11 12:40:41 +0300

    Рефакторинг кода
    Перенос запуска бота в отдельный класс

--- app/main.py
+++ app/main.py
@@ -1,60 +1,15 @@
 import os
 import threading
-from rag.engine import RagEngine
-from telegram_bot import TelegramBotClient
-from vector_db.qdrant_manager import QdrantManager
-from llm.gigachat_client import GigaChatClient
+from telegram_bot import TelegramRunner
 from dotenv import load_dotenv
 
-def run_bot():
-    """Функция для запуска бота в отдельном потоке"""
-    try:
-        qdrant = QdrantManager(host=os.getenv("QDRANT_HOST"))
-        gigachat = GigaChatClient(api_key=os.getenv("GIGACHAT_API_KEY"))
-        rag = RagEngine(qdrant=qdrant, gigachat=gigachat)
-        bot = TelegramBotClient(
-            token=os.getenv("TELEGRAM_BOT_TOKEN"), 
-            gigachat=gigachat, 
-            qdrant=qdrant, 
-            rag=rag
-        )
-        
-        # Запускаем бота
-        bot.run()  # или bot.start()
-        
-    except Exception as e:
-        print(f"❌ Ошибка в потоке бота: {e}")
-        import traceback
-        traceback.print_exc()
+
 
 def main():
     load_dotenv()
+    bot = TelegramRunner()
+    bot.start()
     
-    # Запускаем бота в отдельном потоке
-    bot_thread = threading.Thread(target=run_bot, name="TelegramBot")
-    bot_thread.daemon = True
-    bot_thread.start()
-    
-    print("✅ Телеграм бот запущен в отдельном потоке")
-    print("🚀 Позже здесь можно запустить веб-сервер")
-    print("⏹️  Нажмите Ctrl+C для остановки")
-    
-    # Основной поток ждет
-    try:
-        while True:
-            # Проверяем, жив ли поток с ботом
-            if not bot_thread.is_alive():
-                print("❌ Поток с ботом остановился, перезапускаем...")
-                bot_thread = threading.Thread(target=run_bot, name="TelegramBot")
-                bot_thread.daemon = True
-                bot_thread.start()
-                print("✅ Бот перезапущен")
-            
-            # Ждем немного перед следующей проверкой
-            threading.Event().wait(10)  # Увеличил интервал проверки
-            
-    except KeyboardInterrupt:
-        print("\n👋 Завершение работы...")
 
 if __name__ == "__main__":
     main()
--- app/telegram_bot.py
+++ app/telegram_bot.py
@@ -1,5 +1,64 @@
+import os
+import threading
 from telegram.ext import Application, MessageHandler, filters
+from core.app_contex import AppContext
 from core.agent import DialogAgent
+
+class TelegramRunner:
+
+    bot = None
+
+    def __init__(self):
+        pass
+    
+    def run_bot(self):
+        """Функция для запуска бота в отдельном потоке"""
+        try:
+            qdrant = AppContext.QdrantManager
+            gigachat = AppContext.GigaChatClient
+            rag = AppContext.Rag
+            bot = TelegramBotClient(
+                token=os.getenv("TELEGRAM_BOT_TOKEN"), 
+                gigachat=gigachat, 
+                qdrant=qdrant, 
+                rag=rag
+            )
+            
+            # Запускаем бота
+            bot.run()  # или bot.start()
+            
+        except Exception as e:
+            print(f"❌ Ошибка в потоке бота: {e}")
+            import traceback
+            traceback.print_exc()
+
+    # Альтернативное название метода для ясности
+    def start(self):
+        # Запускаем бота в отдельном потоке
+        bot_thread = threading.Thread(target=self.run_bot, name="TelegramBot")
+        bot_thread.daemon = True
+        bot_thread.start()
+        
+        print("✅ Телеграм бот запущен в отдельном потоке")
+        print("🚀 Позже здесь можно запустить веб-сервер")
+        print("⏹️  Нажмите Ctrl+C для остановки")
+        
+        # Основной поток ждет
+        try:
+            while True:
+                # Проверяем, жив ли поток с ботом
+                if not bot_thread.is_alive():
+                    print("❌ Поток с ботом остановился, перезапускаем...")
+                    bot_thread = threading.Thread(target=self.run_bot, name="TelegramBot")
+                    bot_thread.daemon = True
+                    bot_thread.start()
+                    print("✅ Бот перезапущен")
+                
+                # Ждем немного перед следующей проверкой
+                threading.Event().wait(10)  # Увеличил интервал проверки
+                
+        except KeyboardInterrupt:
+            print("\n👋 Завершение работы...")
 
 class TelegramBotClient:
     def __init__(self, token: str, gigachat, qdrant, rag):
@@ -30,8 +89,3 @@ class TelegramBotClient:
         """Запуск бота (синхронный метод для использования в потоках)"""
         print("Бот запущен...")
         self.bot.run_polling()
-
-    # Альтернативное название метода для ясности
-    def start(self):
-        """Альтернативное название для запуска"""
-        self.run()
